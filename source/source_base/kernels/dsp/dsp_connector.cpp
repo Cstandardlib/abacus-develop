7ae2f1d81f2148cfea3dfeae5b14fd9f2a09a9d8
Chen Nuo <49788094+Cstandardlib@users.noreply.github.com>
2026-01-22 14:03:39 +0800

    Revert "[Feature]  Adapt ABACUS to newest version of mtblas and mtfft (#6548)"
    This reverts commit bc2d28742a67f3694820b804a419504513f56708.

--- source/source_base/kernels/dsp/dsp_connector.cpp
+++ source/source_base/kernels/dsp/dsp_connector.cpp
@@ -6,9 +6,9 @@
 extern "C"
 {
 #define complex_double ignore_complex_double
-#include <mt_hthread_blas.h> // include faster mtblas kernels
+#include <mt_hthread_blas.h> // MTBLAS_TRANSPOSE etc
 #undef complex_double
-#include <mtblas_interface.h> // include normal mtblas kernels that automatically operate memory, but slower.
+#include <mtblas_interface.h> // gemm
 }
 namespace mtfunc
 {
@@ -22,42 +22,45 @@ void dspDestoryHandle(int id)
 {
     hthread_dev_close(id);
     std::cout << " ** DSP closed on cluster " << id << " **" << std::endl;
-} // Close dsp cluster at the end of the program
+} // Close dsp cluster at the end
 
-// MTBlas secretly removed its MTBLAS_TRANSPOSE data type and used the original CBLAS_TRANSPOSE. So this function is modified.
-
-CBLAS_TRANSPOSE convertBLASTranspose(const char* blasTrans)
+MTBLAS_TRANSPOSE convertBLASTranspose(const char* blasTrans)
 {
     switch (blasTrans[0])
     {
     case 'N':
     case 'n':
-        return CblasNoTrans;
+        return MtblasNoTrans;
     case 'T':
     case 't':
-        return CblasTrans;
+        return MtblasTrans;
     case 'C':
     case 'c':
-        return CblasConjTrans;
+        return MtblasConjTrans;
     default:
         std::cout << "Invalid BLAS transpose parameter!! Use default instead." << std::endl;
-        return CblasNoTrans;
+        return MtblasNoTrans;
     }
-} // Used to convert normal transpost char to cblas transpose flag
+} // Used to convert normal transpost char to mtblas transpose flag
 
 void* malloc_ht(size_t bytes, int cluster_id)
 {
+    // std::cout << "MALLOC " << cluster_id;
     void* ptr = hthread_malloc((int)cluster_id, bytes, HT_MEM_RW);
+    // std::cout << ptr << " SUCCEED" << std::endl;;
     return ptr;
-} // Malloc on dsp. Used to replace original malloc
+}
 
-
+// Used to replace original malloc
 
 void free_ht(void* ptr)
 {
+    // std::cout << "FREE " << ptr;
     hthread_free(ptr);
-} // Free on dsp. Used to replace original free
+    // std::cout << " FREE SUCCEED" << std::endl;
+}
 
+// Used to replace original free
 
 void sgemm_mt_(const char* transa,
                const char* transb,
@@ -74,7 +77,7 @@ void sgemm_mt_(const char* transa,
                const int* ldc,
                int cluster_id)
 {
-    mtblas_sgemm(CBLAS_ORDER::CblasColMajor,
+    mtblas_sgemm(MTBLAS_ORDER::MtblasColMajor,
                  convertBLASTranspose(transa),
                  convertBLASTranspose(transb),
                  *m,
@@ -106,7 +109,7 @@ void dgemm_mt_(const char* transa,
                const int* ldc,
                int cluster_id)
 {
-    mtblas_dgemm(CBLAS_ORDER::CblasColMajor,
+    mtblas_dgemm(MTBLAS_ORDER::MtblasColMajor,
                  convertBLASTranspose(transa),
                  convertBLASTranspose(transb),
                  *m,
@@ -138,7 +141,7 @@ void zgemm_mt_(const char* transa,
                const int* ldc,
                int cluster_id)
 {
-    mtblas_zgemm(CBLAS_ORDER::CblasColMajor,
+    mtblas_zgemm(MTBLAS_ORDER::MtblasColMajor,
                  convertBLASTranspose(transa),
                  convertBLASTranspose(transb),
                  *m,
@@ -170,7 +173,7 @@ void cgemm_mt_(const char* transa,
                const int* ldc,
                int cluster_id)
 {
-    mtblas_cgemm(CBLAS_ORDER::CblasColMajor,
+    mtblas_cgemm(MTBLAS_ORDER::MtblasColMajor,
                  convertBLASTranspose(transa),
                  convertBLASTranspose(transb),
                  *m,
@@ -204,7 +207,7 @@ void sgemm_mth_(const char* transa,
                 const int* ldc,
                 int cluster_id)
 {
-    mt_hthread_sgemm(CBLAS_ORDER::CblasColMajor,
+    mt_hthread_sgemm(MTBLAS_ORDER::MtblasColMajor,
                      convertBLASTranspose(transa),
                      convertBLASTranspose(transb),
                      *m,
@@ -236,7 +239,7 @@ void dgemm_mth_(const char* transa,
                 const int* ldc,
                 int cluster_id)
 {
-    mt_hthread_dgemm(CBLAS_ORDER::CblasColMajor,
+    mt_hthread_dgemm(MTBLAS_ORDER::MtblasColMajor,
                      convertBLASTranspose(transa),
                      convertBLASTranspose(transb),
                      *m,
@@ -272,7 +275,7 @@ void zgemm_mth_(const char* transa,
     *alp = *alpha;
     std::complex<double>* bet = (std::complex<double>*)malloc_ht(sizeof(std::complex<double>), cluster_id);
     *bet = *beta;
-    mt_hthread_zgemm(CBLAS_ORDER::CblasColMajor,
+    mt_hthread_zgemm(MTBLAS_ORDER::MtblasColMajor,
                      convertBLASTranspose(transa),
                      convertBLASTranspose(transb),
                      *m,
@@ -311,7 +314,7 @@ void cgemm_mth_(const char* transa,
     std::complex<float>* bet = (std::complex<float>*)malloc_ht(sizeof(std::complex<float>), cluster_id);
     *bet = *beta;
 
-    mt_hthread_cgemm(CBLAS_ORDER::CblasColMajor,
+    mt_hthread_cgemm(MTBLAS_ORDER::MtblasColMajor,
                      convertBLASTranspose(transa),
                      convertBLASTranspose(transb),
                      *m,
